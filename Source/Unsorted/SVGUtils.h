52c0d49629127f337b595a75dfab56918d08022d
Andrey Vakunov <wako.an@gmail.com>
2014-12-11 21:58:56 +0300

    add isOpacitySetted to header

--- Source/Unsorted/SVGUtils.h
+++ Source/Unsorted/SVGUtils.h
@@ -24,6 +24,7 @@ typedef struct {
 
 SVGColor SVGColorMake (uint8_t r, uint8_t g, uint8_t b, uint8_t a);
 SVGColor SVGColorFromString (const char *string);
+BOOL isOpacitySetted (const char *string);
 
 CGFloat SVGPercentageFromString (const char *string);
 
